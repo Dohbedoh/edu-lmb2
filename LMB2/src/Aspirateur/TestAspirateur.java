9f544a654cd9cce33ca9b25e4c41f3256950afec
loafer54 <loafer54@5c89688c-be40-11de-a8ef-132414f8baa6>
2009-10-28 14:17:44 +0000

    Voici la première mise à jour. Elle à lieu quand on execute un commit ! Il ne faut pas oublier de selectionner les fichiers a mettre à jour !!

---
+++ LMB2/src/Aspirateur/TestAspirateur.java
@@ -0,0 +1,8 @@
+package Aspirateur;
+
+public class TestAspirateur {
+
+	public static void main (String[] args){
+		System.out.println("Si tu vois ce texte, c'est que tu as bien suivi les instructions");
+	}
+}
