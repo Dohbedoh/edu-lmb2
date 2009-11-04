505b1272e3db3cdcfd31f831344857fc7dd24a87
grogro57 <grogro57@5c89688c-be40-11de-a8ef-132414f8baa6>
2009-11-04 15:30:58 +0000

    Librairie Magique "HTMLParser" et exemple avec le site créé par Greg.

--- LMB2/src/Aspirateur/TestAspirateur.java
+++ LMB2/src/Aspirateur/TestAspirateur.java
@@ -1,10 +1,27 @@
 package Aspirateur;
 
+import org.htmlparser.Parser;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
+
 public class TestAspirateur {
 
+	Parser parser;
+	
 	public static void main (String[] args){
 		System.out.println("Si tu vois ce texte, c'est que tu as bien suivi les instructions");
 		System.out.println("J'ai bien suivi les instructions mon p'tit!");
 		System.out.println("J'ai bien suivi les instructions mon p'tit!, by Greg");
-	}
+        try
+        {
+            Parser parser = new Parser ("http://www.renaudmathieu.fr/lmb2/");
+            NodeList list = parser.parse (null);
+            System.out.println (list.toHtml ());
+        }
+        catch (ParserException pe)
+        {
+            pe.printStackTrace ();
+        }
+    }
+
 }
