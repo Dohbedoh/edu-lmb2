64a1a4abddaf3ccc420394e25ab1abca6071e4f4
grogro57 <grogro57@5c89688c-be40-11de-a8ef-132414f8baa6>
2009-11-06 21:19:56 +0000

    SiteCapturer est un exemple d'utilisation de la librairie "HTMLParser" dont la source de trouve sur "http://htmlparser.sourceforge.net/". Il s'agit maintenant de bien regarder cette exemple pour comprendre comment parser correctement un site HTML...

--- LMB2/src/Aspirateur/TestAspirateur.java
+++ LMB2/src/Aspirateur/TestAspirateur.java
@@ -1,27 +1,43 @@
 package Aspirateur;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+
+import org.htmlparser.Node;
+import org.htmlparser.NodeFilter;
 import org.htmlparser.Parser;
 import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
+import org.htmlparser.util.SimpleNodeIterator;
 
-public class TestAspirateur {
 
+
+public class TestAspirateur{
+
+	NodeList list;
 	Parser parser;
+	ArrayList<URL> urlVisited;
+	
+	
+	public TestAspirateur(String url){
+		parser = new Parser();
+		try {
+			parser.setURL(url);
+			list = parser.parse(null);
+			System.out.println(list.toHtml());
+			
+		} catch (ParserException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void remplirURLs(NodeList list){
+		
+	}
 	
 	public static void main (String[] args){
-		System.out.println("Si tu vois ce texte, c'est que tu as bien suivi les instructions");
-		System.out.println("J'ai bien suivi les instructions mon p'tit!");
-		System.out.println("J'ai bien suivi les instructions mon p'tit!, by Greg");
-        try
-        {
-            Parser parser = new Parser ("http://www.renaudmathieu.fr/lmb2/");
-            NodeList list = parser.parse (null);
-            System.out.println (list.toHtml ());
-        }
-        catch (ParserException pe)
-        {
-            pe.printStackTrace ();
-        }
-    }
-
+		TestAspirateur test = new TestAspirateur("http://localhost/Projet%20CESI/page_cnx.html");
+	}
 }
