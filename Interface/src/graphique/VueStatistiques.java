f85ca9e385b3f1c3b97530c7ac86fa42491e41a1
loafer54 <loafer54@5c89688c-be40-11de-a8ef-132414f8baa6>
2009-11-20 16:07:36 +0000

    Ca avance doucement !!
    PS : on n'a pas importé notre travail pour le moment

---
+++ Interface/src/graphique/VueStatistiques.java
@@ -0,0 +1,34 @@
+/**
+ * @author BESLUAU Gregoire, BURDAJEWICZ Allan, LARAKI Meryem, MATHIEU Renaud
+ */
+
+package graphique;
+
+import Aspirateur.*;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class VueStatistiques extends JPanel{
+
+	//------------------
+	// Attributs
+	//------------------
+	public TestAspirateur laspirateur;
+	
+	public JLabel afficheURL;
+	
+	//------------------
+	// Constructeurs
+	//------------------
+	public VueStatistiques(TestAspirateur laspirateur){
+		this.laspirateur = laspirateur;
+		
+	}//cons-1
+	
+	//------------------
+	// Methodes
+	//------------------
+}
