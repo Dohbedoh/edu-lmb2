65bf3b2cbdce2189f6dc56fb1609a29d947c4a98
loafer54 <loafer54@5c89688c-be40-11de-a8ef-132414f8baa6>
2010-01-09 16:01:59 +0000

    Mise en place du Look&Feel !

--- LMB2/src/graphique/Main.java
+++ LMB2/src/graphique/Main.java
@@ -4,6 +4,8 @@
 
 package graphique;
 
+import javax.swing.*;
+
 import Aspirateur.Aspirateur;
 
 public class Main {
@@ -15,8 +17,20 @@ public class Main {
 	 * Lancement du programme principal
 	 */
 	public static void main(String[] args){
-		Aspirateur laspirateur = new Aspirateur();
-		new VueChoixWorkspace(laspirateur);
+		JFrame.setDefaultLookAndFeelDecorated(true);
+	    SwingUtilities.invokeLater(new Runnable() {
+	      public void run() {
+	        try {
+	        	//UIManager.setLookAndFeel("org.jvnet.substance.skin.SubstanceOfficeSilver2007LookAndFeel");
+	        	UIManager.setLookAndFeel("org.jvnet.substance.skin.SubstanceOfficeBlue2007LookAndFeel");
+	        } catch (Exception e) {
+	          System.out.println("Substance Raven Graphite failed to initialize");
+	        }
+	        Aspirateur laspirateur = new Aspirateur();
+			new VueChoixWorkspace(laspirateur);
+	      }
+	    });
+		
 	}
 	
 }
